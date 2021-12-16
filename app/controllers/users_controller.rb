d759afdb8909915996310fe0ae1104bffd22cddf
Hanada-Takao <taaa0823@gmail.com>
2021-12-16 18:37:17 +0900

    Profile_access_restrictions_mountain

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
-    skip_before_action :login_required, only: [:new, :edit, :show, :create]
+    skip_before_action :login_required, only: [:new]
+    before_action :logged_in_user, only: [:edit, :update]
+    before_action :correct_user,   only: [:edit, :update]
+
     def new
         @user = User.new
     end
@@ -32,10 +35,23 @@ class UsersController < ApplicationController
       end
     end
 
+    private
+
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                     :password_confirmation)
     end
 
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(new_session_path) unless @user == current_user
+    end
 
 end
